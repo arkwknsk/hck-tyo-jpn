7c3ea427c14cdd393e3fc9372c1ac0a1ca4ec7a9
Kensuke ARAKAWA <mail@arakawakensuke.jp>
2022-12-02 21:47:46 +0900

    fix: CSSとPIXI.js側でのスケールのズレを是正

--- src/Context.ts
+++ src/Context.ts
@@ -3,6 +3,10 @@ export class Context {
     public static STAGE_WIDTH: number = 11012;
     public static STAGE_HEIGHT: number = 1080;
 
+    public static NUMBER_MAPS: number = 12
+    public static MAP_WIDTH = 300
+    public static MAP_HEIGHT = 300
+
     private static _instance: Context;
 
     /**
